4a012b1c4056f7f661d001d681d3e2a59973933c
nhatktvn2001 <nhatktvn2001@gmail.com>
2022-09-11 19:12:54 +0700

    first test

---
+++ src/new.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')  // Nạp thư viện express vừa cài
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const app = express()
+const port = 3000
+const route = require('./routes')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))  // HTTP logger
+app.engine('hbs', engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+
+route(app)
+app.get('/home', function(req,res) {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
